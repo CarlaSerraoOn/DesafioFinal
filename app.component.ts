29cb027a38e85a5a4d7d4a22c955e6a169a89c30
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-02-01 22:34:51 -0300

    Update 1.7

--- app.component.ts
+++ app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 import {WebcamImage} from 'ngx-webcam';
 import {Subject, Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,9 @@ export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
+
+  constructor(private http: HttpClient) {}
+
   triggerObservable = new Subject();
     handleImage(webcamImage: WebcamImage) {
         console.log('received webcam image', webcamImage);
@@ -22,19 +26,30 @@ export class AppComponent {
         stopButtonClass = 'stop-button-red';
         startLed = false;
         stopLed = false;
-        
+
         start() {
         this.startLed = true;
         this.stopLed = false;
         this.startButtonClass = 'start-button-dark-green';
         this.stopButtonClass = 'stop-button-red';
+        this.http.get('http://localhost:3000/on').subscribe(data => {
+      console.log(data);
+    });
         }
-        
+
         stop() {
         this.startLed = false;
         this.stopLed = true;
         this.startButtonClass = 'start-button-green';
         this.stopButtonClass = 'stop-button-dark-red';
+        this.http.get('http://localhost:3000/off').subscribe(data => {
+      console.log(data);
+    });
         }
+
+
+
+
+
         }
-  
+
