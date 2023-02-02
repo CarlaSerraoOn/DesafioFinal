2343c3fdce4695b0836cd45e9b37bc20c263cc32
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-02-01 22:33:36 -0300

    Update 1.7

--- app.module.ts
+++ app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
 import { WebcamModule } from 'ngx-webcam';
+
 
 @NgModule({
   declarations: [
@@ -13,12 +15,10 @@ import { WebcamModule } from 'ngx-webcam';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule
+    WebcamModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
