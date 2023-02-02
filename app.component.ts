c98a15fdc8e9c264e0cb2a6c8a224384dc114791
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-02-02 16:38:06 -0400

    Update 1.8

--- app.component.ts
+++ app.component.ts
@@ -1,9 +1,15 @@
 // app.component.ts
-import { Component } from '@angular/core';
+import { Component,} from '@angular/core';
+
 
 import {WebcamImage} from 'ngx-webcam';
 import {Subject, Observable} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
+type resultadoCor = {
+  cor:string,
+  imagem:string
+}
 
 @Component({
   selector: 'app-root',
@@ -11,11 +17,25 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
+  imageUrl: string | undefined;
+  cor: string | undefined;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  this.getImage();
+  setInterval(() => {
+    this.getImage();
+  }, 3000);
+}
+
+getImage() {
+  this.http.get<resultadoCor>('http://10.113.163.132:5000/getcolor').subscribe((data:resultadoCor) => {
+    this.imageUrl = 'data:image/png;base64,' + data.imagem;
+    console.log(data.imagem);
+    this.cor = data.cor;
+  });
+}
+
+  
 
   triggerObservable = new Subject();
     handleImage(webcamImage: WebcamImage) {
@@ -52,4 +72,3 @@ export class AppComponent {
 
 
         }
-
